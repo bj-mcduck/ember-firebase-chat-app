0696d5c5a43231b347c4a627a07219d9d9219208
Brandon J McKay <admin@brandonjmckay.com>
2015-07-29 07:10:34 -0700

    assign random avatar to user
    When a user signs up, they'll be assigned a random avatar from the list
    of Arrested Development pics.
    
    I also added a .save() to the end of the call so that it will propagate
    to the server immediately.

--- app/pods/signup/controller.js
+++ app/pods/signup/controller.js
@@ -5,12 +5,30 @@ import LoginUser from 'ember-firebase-chat-app/mixins/login-user';
 export default Ember.Controller.extend(
   LoginUser, {
 
+  avatars: [
+    'buster.jpg',
+    'buster2.jpg',
+    'george-michael.jpg',
+    'george-michael2.jpg',
+    'george.jpg',
+    'gob3.jpg',
+    'lindsay.jpg',
+    'maeby.png',
+    'michael.jpg',
+    'michael2.png'
+  ],
+
   userValues(){
     return {
       email: this.get('email'),
       password: this.get('password'),
       passwordConfirmation: this.get('passwordConfirmation')
     };
+  },
+
+  getRandomAvatar(){
+    var randomNumber = Math.round(Math.random());
+    return this.get('avatars')[randomNumber];
   },
 
   actions: {
@@ -21,10 +39,11 @@ export default Ember.Controller.extend(
           //Handle Errors here.
           console.log(error);
         } else {
-          var user = this.store.createRecord('user', {
+          this.store.createRecord('user', {
             id: userData.uid,
-            name: this.get('name')
-          });
+            name: this.get('name'),
+            avatar: this.getRandomAvatar()
+          }).save();
           this.authenticateUser(this.get('email'), this.get('password'));
         }
       }.bind(this));
