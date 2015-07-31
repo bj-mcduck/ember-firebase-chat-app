1315cfb56d95130b827fd27fd2ed644af3dedb89
Brandon J McKay <admin@brandonjmckay.com>
2015-07-31 13:49:45 -0700

    Add toasts for user connections
    When a user connects, a toast will display showing them that they've
    connected, or disconnected.

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -1,19 +1,49 @@
 import Ember from 'ember';
+import config from 'ember-firebase-chat-app/config/environment';
 const {Controller, computed} = Ember;
 
 export default Controller.extend({
   currentUser: computed('session.secure.uid', function(){
-    var uid = this.get('session.secure.uid');
+    const uid = this.get('session.secure.uid');
     if (uid !== undefined) {
-      return this.store.findRecord('user', uid);
+      this.setUpPresenceCheck(uid);
+      this.toast("You've logged in.");
+      return this.store.find('profile', uid);
     } else {
       return null;
     }
   }),
 
+  setUpPresenceCheck(uid){
+    const isConnected = new Firebase(config.firebase + '/.info/connected');
+    isConnected.on('value', function(snapshot){
+      if (snapshot.val()){
+        let userRef = new Firebase(config.firebase + '/profiles/' + uid + '/online');
+        userRef.onDisconnect(function(){
+          this.toast("You've logged out.");
+          userRef.set(Firebase.ServerValue.TIMESTAMP);
+        }.bind(this));
+        userRef.set(true);
+      }
+    });
+  },
+
+  toast(message){
+    this.set('toastMessage', message);
+    this.set('showToast', true);
+  },
+
   actions: {
     goTo(route){
       this.transitionToRoute(route);
+    },
+
+    toast(message){
+      this.toast(message);
+    },
+
+    closeToast(){
+      this.set('showToast', false);
     }
   }
 });
