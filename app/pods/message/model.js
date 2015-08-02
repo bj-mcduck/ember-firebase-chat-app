ce831b438d66dd667e3c7c641ee40b9c478c8c49
Brandon J McKay <admin@brandonjmckay.com>
2015-08-02 08:24:03 -0700

    add a createdAt attribute to messages

--- app/pods/message/model.js
+++ app/pods/message/model.js
@@ -2,7 +2,8 @@ import DS from 'ember-data';
 const {attr, belongsTo, Model} = DS;
 
 export default Model.extend({
-  content: attr('string'),
+  content:   attr('string'),
+  createdAt: attr('number'),
 
   user: belongsTo('user', {
     inverse: 'messages',
